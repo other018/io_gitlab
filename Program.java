cd46c1abc6fe773d17e1b28d8af6c1444817eee6
unknown <inf136835@lab-45-10.cs.put.poznan.pl>
2019-11-06 10:32:57 +0100

    pierwszy

--- Program.java
+++ Program.java
@@ -1,7 +1,9 @@
 //Klasa Hello World
 public class Program {
+  private int tmp;
   public int zmienna;
   public static void main(String[] args){
+    System.out.print("Hey");
     System.out.print("Hello World");
   }
   public static void smth ()
