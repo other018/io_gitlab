44be3061764b9960e2b6ecd649af174308b4929f
unknown <inf136835@lab-45-10.cs.put.poznan.pl>
2019-11-06 10:30:11 +0100

    zmienna

--- Program.java
+++ Program.java
@@ -1,5 +1,10 @@
 public class Program {
+  public int zmienna;
   public static void main(String[] args){
-    System.out.print("Hello World Today");
+    System.out.print("Hello World");
+  }
+  public static void smth ()
+  {
+    System.out.print("Another Hello");
   }
 }
